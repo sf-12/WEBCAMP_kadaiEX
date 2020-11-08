8397dab4b69f633db36b4a1d977e9fc661d3969e
sf-12 <s.fukasawa12@gmail.com>
2020-11-08 23:21:29 +0000

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'user/index'
+  get 'user/show'
   devise_for :users
 
   root to:'homes#top'
-  # resources :homes, only: [:show]
-  get 'about', to: 'homes#about'
+  get 'home/about', to: 'homes#about'
+  # resources :users, only: [:show]
+  resources :users, only: [:index, :show]
+  resources :books, only: [:index, :show]
 end
