1d1e8af696b7bc983dd6a86c0e12522eca1d1d02
sf-12 <s.fukasawa12@gmail.com>
2020-11-08 06:43:48 +0000

    add SignUp Login Logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # devise使用時にメソッドを呼ぶ
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    # ユーザ登録時にユーザ名(name) の操作を許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # ログイン時にユーザ名(name) の操作を許可する
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
 end
