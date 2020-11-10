047cb7e4fba26a8d6be0d397900e4150de373c59
sf-12 <s.fukasawa12@gmail.com>
2020-11-10 09:26:27 +0000

    add user edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
   get 'home/about', to: 'homes#about'
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
 end
