947ecf6e6efda8c05f444ae4b351076c6303f725
sf-12 <s.fukasawa12@gmail.com>
2020-12-02 11:14:20 +0000

    [Finish] 応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,33 +1,25 @@
 class BookCommentsController < ApplicationController
   def create
     # 登録する
+    @book = Book.find(params[:book_id])
+    @book_comments = BookComment.where(book_id: @book.id)
     @new_comment = BookComment.new(comment_params)
     @new_comment.user_id = current_user.id
-    @new_comment.book_id = params[:book_id]
-    if @new_comment.save
-      # 元の画面に遷移する
-      redirect_to request.referer
-    else
-      # 元の画面に遷移する
-      # バリデーションエラーを表示しようとすると、URL変化が厄介なので暫定でエラー非表示
-      # @book = Book.find(params[:book_id])
-      # @user = User.find_by(id: @book.user_id)
-      # @book_comments = BookComment.where(book_id: @book.id)
-      # render template: 'books/show'
-      redirect_to request.referrer
-    end
-
+    @new_comment.book_id = @book.id
+    @new_comment.save
+    # 元の画面に遷移する
+    # redirect_to request.referer
+    # バリデーションエラーを表示しようとすると、URL変化が厄介なので暫定でエラー非表示
   end
 
   def destroy
     # 削除する
+    @book = Book.find(params[:book_id])
+    @book_comments = BookComment.where(book_id: @book.id)
     @book_comment = BookComment.find_by(id: params[:id])
-    p params[:id]
-    p params[:book_id]
-    p @book_comment
     @book_comment.delete
     # 元の画面に遷移する
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   private
