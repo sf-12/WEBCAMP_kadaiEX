b396b57eb8bf051d6bf24cc015f4cd4f8597eac0
sf-12 <s.fukasawa12@gmail.com>
2020-12-02 07:43:19 +0000

    asynchronous communication of favorite button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,8 @@
 class FavoritesController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
     # ボタン連打しても1回しか"いいね"できないように"いいね"してない時のみ処理
-    if not Book.find(params[:book_id]).favorited_by?(current_user)
+    if not @book.favorited_by?(current_user)
       # 登録する
       @favorite = Favorite.new
       @favorite.user_id = current_user.id
@@ -9,18 +10,19 @@ class FavoritesController < ApplicationController
       @favorite.save
     end
     # 元の画面に遷移する
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     # ボタン連打しても1回しか"いいね"解除できないように"いいね"済みの時のみ処理
-    if Book.find(params[:book_id]).favorited_by?(current_user)
+    if @book.favorited_by?(current_user)
       # 削除する
       @favorite = Favorite.find_by(user_id: params[:id], book_id: params[:book_id])
       @favorite.delete
     end
     # 元の画面に遷移する
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
