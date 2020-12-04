f223b0db1e28ed1d82e96c3db0a50bda451b883c
sf-12 <s.fukasawa12@gmail.com>
2020-12-04 06:32:34 +0000

    [Finish] 応用課題9

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,14 +1,18 @@
 class ChatsController < ApplicationController
 
   def create
+
+    # JavaScriptに渡すインスタンス変数
+    @chats = Chat.where(room_id: params[:room_id])
+    @partner_user = User.find(params[:user_id])
+    @new_chat = Chat.new
+    @room = Room.find(params[:room_id])
+
     # chat作成
-    p "--------------------====================="
     chat = Chat.new(chat_params)
     chat.user_id = current_user.id
     chat.room_id = params[:room_id]
     chat.save
-    # 元の画面に戻る
-    redirect_to user_room_path(params[:user_id], params[:room_id])
   end
 
   private
